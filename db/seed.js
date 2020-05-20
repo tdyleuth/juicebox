931d30015efbf38699f49aa3dcde69c2629916e8
tdyleuth <tony.dyleuth@gmail.com>
2020-05-19 21:07:47 -0700

    Part 2 Webservers Day_2 Protecting Our Secrets

--- db/seed.js
+++ db/seed.js
@@ -188,10 +188,6 @@ async function createInitialPosts() {
       }
     }
     
-<<<<<<< HEAD
-
-=======
->>>>>>> 47a943005df1d82a786bf7ccc3df03026cca9345
 
 async function rebuildDB() {
   try {
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const PORT = 3000;
 const express = require('express');
 const server = express();
