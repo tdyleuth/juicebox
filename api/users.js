4607ab0279e6203aee21900d7c4ba18e3b8c9a01
tdyleuth <tony.dyleuth@gmail.com>
2020-05-21 21:50:29 -0700

    Day_2 Web Servers Create Users

--- api/users.js
+++ api/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const usersRouter = express.Router();
-const { getAllUsers, getUserByUsername } = require('../db');
+const { getAllUsers, getUserByUsername, createUser } = require('../db');
+
+const jwt = require('jsonwebtoken')
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -32,10 +34,16 @@ usersRouter.post('/login', async (req, res, next) => {
   
     try {
       const user = await getUserByUsername(username);
+
+      const { id } = user;
+
+      //Check if user exists and, if so, if entered password matches saved password in db
   
       if (user && user.password == password) {
         // create token & return to user
-        res.send({ message: "you're logged in!" });
+        const token = jwt.sign({ username, password, id }, process.env.JWT_SECRET)
+  
+        res.send({ message: "You're logged in!", token });
       } else {
         next({ 
           name: 'IncorrectCredentialsError', 
@@ -47,6 +55,45 @@ usersRouter.post('/login', async (req, res, next) => {
       next(error);
     }
   });
+
+  
+  usersRouter.post('/register', async (req, res, next) => {
+    const { username, password, name, location } = req.body;
+  
+    try {
+      const _user = await getUserByUsername(username);
+  
+      if (_user) {
+        next({
+          name: 'UserExistsError',
+          message: 'A user by that username already exists'
+        });
+      }
+  
+      const user = await createUser({
+        username,
+        password,
+        name,
+        location,
+      });
+  
+      const token = jwt.sign({ 
+        id: user.id, 
+        username
+      }, process.env.JWT_SECRET, {
+        expiresIn: '1w'
+      });
+  
+      res.send({ 
+        message: "thank you for signing up",
+        token 
+      });
+    } catch ({ name, message }) {
+      next({ name, message })
+    } 
+  });
+
+
 
 
 
