bf4c952f6515e4878754b9ff73c0cd8666ff2a55
tdyleuth <tony.dyleuth@gmail.com>
2020-05-23 12:33:04 -0700

    Push to Heruku Master

--- db/index.js
+++ db/index.js
@@ -1,7 +1,6 @@
 const { Client } = require('pg');
 
-const client = new Client('postgres://localhost:5432/juicebox-dev');
-
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 async function getAllUsers() {
     const { rows } = await client.query(
